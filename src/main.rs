ad31112d45c8fd09970f0271a8705b9bc4a274fd
Ryp <ryp.sqrt@gmail.com>
2019-07-09 09:09:05 +0200

    clean: refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@ extern crate clap;
 use clap::{Arg, App};
 
 fn main() {
-    let matches = App::new("chip8-emu-rs")
+    // Argument parsing
+    let matches = App::new("CHIP-8 Emulator")
         .arg(Arg::with_name("rom_path")
              .help("path of the CHIP-8 ROM to load")
              .required(true)
@@ -19,38 +20,30 @@ fn main() {
              .short("d")
              .help("enable debug mode"))
         .arg(Arg::with_name("scale")
-             .takes_value(true)
              .short("s")
+             .takes_value(true)
              .help("screen upscale factor"))
         .get_matches();
 
     let rom_path = matches.value_of("rom_path").unwrap();
-    let screen_scale = value_t!(matches, "scale", u32).unwrap_or(16);
-    let debug_mode = matches.is_present("debug");
-
-    let mut state: chip8::CPUState = chip8::create_chip8_state();
-
-    // Load program in chip8 memory
-    {
-        println!("[INFO] loading program: {:?}", rom_path);
-
-        let mut rom_file = File::open(&Path::new(&rom_path)).expect("Could not open file");
-        let mut rom_content = Vec::<u8>::new();
-        rom_file.read_to_end(&mut rom_content).expect("Unable to read the file");
-
-        println!("[INFO] program loaded");
-
-        chip8::load_program(&mut state, rom_content);
-    }
 
     let config = chip8::EmuConfig {
-        debug_mode: debug_mode,
+        debug_mode: matches.is_present("debug"),
         palette: chip8::Palette {
             primary: chip8::Color { r: 1.0, g: 1.0, b: 1.0 },
             secondary: chip8::Color { r: 0.14, g: 0.14, b: 0.14 }
         },
-        screen_scale: screen_scale
+        screen_scale: value_t!(matches, "scale", u32).unwrap_or(16),
     };
+
+    let mut state: chip8::CPUState = chip8::create_chip8_state();
+
+    // Load program in emu memory
+    let mut rom_file = File::open(&Path::new(&rom_path)).expect("Could not open file");
+    let mut rom_content = Vec::<u8>::new();
+    rom_file.read_to_end(&mut rom_content).expect("Unable to read the file");
+
+    chip8::load_program(&mut state, rom_content);
 
     sdl2::execute_main_loop(&mut state, &config).unwrap();
 }
